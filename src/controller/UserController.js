6182ee92175fc42a85de31a5a3664532913cfc38
Bruna Silva <bubysilva_8@hotmail.com>
2021-09-30 23:19:52 -0300

    Finalização do CRUD e utilização do CORS

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,21 +1,9 @@
 const crypto = require('crypto')
 const connection = require('../database/connection')
+const { routes } = require('../routes')
 
 module.exports = {
-    
-    async create(req,res) {
-    const id = crypto.randomBytes(4).toString('hex')
-    const {nome, email, idade, empresa} = req.body
-    await connection('users').insert({
-        id,
-        nome,
-        email,
-        idade,
-        empresa
-    });
-    console.log(id);
-    res.json();
-    },
+
     async list(req,res) {
         const users = await connection('users').select('*');
         res.json(users);
@@ -24,6 +12,36 @@ module.exports = {
         const {id} = req.params;
         const user = await connection('users').where('id', id).select('*');
         return res.json(user);
+    },
+    async create(req,res) {
+        const id = crypto.randomBytes(4).toString('hex')
+        const {nome, email, idade, empresa} = req.body
+        await connection('users').insert({
+            id,
+            nome,
+            email,
+            idade,
+            empresa
+        });
+        console.log(id);
+        res.json();
+    },
+    async update(req,res){
+        const {id} = req.params;
+        const {nome,email,idade, empresa} = req.body;
+        await connection('users').where('id',id).update({
+            id,
+            nome,
+            email,
+            idade,
+            empresa
+        })
+        return res.status(204).send();
+    },
+    async delete(req, res){
+        const {id} = req.params;
+        await connection('users').where('id',id).delete();
+        return res.status(204).send();
     }
 
 }
